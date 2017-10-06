962c8921f68495544d5551b9e958a5fff8e776d0
Gonzaga <phellipegonzaga@gmail.com>
2017-10-06 11:02:31 -0300

    tp_tecweb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import { Injectable } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
-}
+
+@Injectable()
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule, Http, Response } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { EmpregoService } from '../services/emprego.service';
+import { RouterModule, Routes } from '@angular/router';
+import { EmpregoPesquisarComponent } from './emprego-pesquisar/emprego-pesquisar.component';
+
+import { AgmCoreModule } from '@agm/core';
+
+const appRoutes: Routes = [
+  { path: 'emprego', component: EmpregoPesquisarComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent, EmpregoPesquisarComponent],
+
+  imports: [BrowserModule, HttpModule, FormsModule,
+    RouterModule.forRoot(appRoutes), AgmCoreModule.forRoot({ apiKey: 'AIzaSyAaYk-47711DvR5617oV2jy0Dn3VAY2RfY' })],
+
+  providers: [AppComponent, EmpregoService],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
